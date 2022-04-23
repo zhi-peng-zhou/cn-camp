93a883e2f643c40d19ee85f6ea26383c2e2cd79c
zhi-peng-zhou <hey_jepen@163.com>
2022-04-23 15:32:27 +0800

    cn-camp-work1: httpserver

---
+++ cn-camp-work1/go.mod
@@ -0,0 +1,5 @@
+module cncamp.work1.com
+
+go 1.17
+
+require github.com/thinkeridea/go-extend v1.3.2 // indirect
---
+++ cn-camp-work1/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/thinkeridea/go-extend/exnet"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+)
+var xlogger = log.Default()
+
+func logHandler(h http.Handler) http.Handler {
+	fu := func(w http.ResponseWriter, r *http.Request) {
+		// 原始请求
+		h.ServeHTTP(w, r)
+		reqUrl := r.URL.String()
+		method := r.Method
+		//t:= reflect.TypeOf(w)
+		vals := reflect.ValueOf(w)
+		iv := reflect.Indirect(vals)
+		statusCode := iv.FieldByName("status")
+		clientIp := exnet.ClientIP(r)
+		logStr := fmt.Sprintf("[%s]:%s  statusCode:%d  ClientIp:%s  ",method, reqUrl,
+		statusCode.Int(), clientIp)
+		xlogger.Println(logStr)
+	}
+	return http.HandlerFunc(fu)
+}
+
+
+func main() {
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/header", setRequestHeader)
+	mux.HandleFunc("/version", getVersion)
+	handler := logHandler(mux)
+	server := http.Server{
+		Addr:":8080",
+		Handler: handler,
+	}
+	server.ListenAndServe()
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "system is healthy, status:200")
+}
+
+func setRequestHeader(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		xlogger.Println(exnet.ClientIP(r))
+		xlogger.Println()
+	}()
+	for k := range r.Header {
+		w.Header().Add(k, r.Header.Get(k))
+	}
+	reqHeader, _ := json.Marshal(r.Header)
+	respHeader, _ := json.Marshal(w.Header())
+	io.WriteString(w, "reqHeader: "+fmt.Sprint(string(reqHeader)+"\n"))
+	io.WriteString(w, "respHeader: "+fmt.Sprint(string(respHeader)+"\n"))
+}
+
+func getVersion(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "the version is "+os.Getenv("VERSION"))
+}
